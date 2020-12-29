ae5be82d39eea29971884d07243f81f414a150c0
hayas1 <h4ystack@gmail.com>
2020-12-29 17:37:11 +0900

    implement sub function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! ghtest_project
+//! # Document
+//! 自動でドキュメントを作成して公開するテストです。
+
+/// a - b を計算します
+/// # Panics
+/// a < b の時、オーバーフローします
+/// # Examples
+/// ```
+/// use crate::ghtest_project::sub;
+/// let (a, b) = (3, 2);
+/// assert_eq!(sub(a, b), 1);
+/// ```
+pub fn sub(a: u64, b: u64) -> u64 {
+    a - b
+}
+
 #[cfg(test)]
 mod tests {
     #[test]

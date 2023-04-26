b26e9dd1d0000ac659b5b032fadfd643840adb67
ryoqun <ryoqun@users.noreply.github.com>
2023-04-26 05:31:22 +0000

    Remove preview for PR 2 🛬

--- pr-preview/pr-2/ghtest_project/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":[["add",""],["sub","a - b を計算します"]]};
--- pr-preview/pr-2/source-files.js
+++
@@ -1,4 +0,0 @@
-var sourcesIndex = JSON.parse('{\
-"ghtest_project":["",[],["lib.rs"]]\
-}');
-createSourceSidebar();

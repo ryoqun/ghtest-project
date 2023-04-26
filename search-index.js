9d5ef618b8a8669ea2c0df9fc9d0f7c9cad000d9
ryoqun <ryoqun@users.noreply.github.com>
2023-04-26 03:10:57 +0000

    deploy: 7032faa1c656b594169ca6b9815642906d770880

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"ghtest_project":{"doc":"ghtest_project","t":[5,5],"n":["add","sub"],"q":["ghtest_project",""],"d":["a + b を計算します","a - b を計算します"],"i":[0,0],"f":[[[1,1],1],[[1,1],1]],"p":[[15,"u64"]]}\
+"ghtest_project":{"doc":"ghtest_project","t":[5,5],"n":["add","sub"],"q":["ghtest_project",""],"d":["","a - b を計算します"],"i":[0,0],"f":[[[1,1],1],[[1,1],1]],"p":[[15,"u64"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};

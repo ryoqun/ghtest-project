0ba6fb2ecd5d2648c0b596360653b888e45644e7
Ryo Onodera <ryoqun@gmail.com>
2023-04-26 17:04:45 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! ghtest_project
 //! # Document
-//! 自動でドキュメントを作成して公開するテストです。yay push ayayayay
+//! 自動でドキュメントを作成して公開するテストです。yay push ayayayay test from forks
 
 #[cfg_attr(doc, aquamarine::aquamarine)]
 /// ```mermaid
